9146707f2dbbc4ecff919432d447a48297ce9209
nicoWSL <nbelfis@gmail.com>
2024-04-18 19:12:20 +0000

    description

--- readme.md
+++ readme.md
@@ -1,58 +1,78 @@
-## build artifacts
-#### build the consumer
-to build the consumer executable
-``./gradlew clean build -PcustomName=consumer -PmainClass=org.example.OneMinuteWindowKstream``
+# Bitcoin volume calculation realtime - (end project Datazoomcamp 2024)
+
+## Problem statement
+
+We want to calculate the volume of "BUY" and "SELL" transaction of bitcoin for specific period of times ( per minutes, hours, days...).
+
+The collection of transaction can be done from different sources of data. In this Exemple we use Coinbase which is one of the largest centralized exchange of cryptocurrencies.
+
+One of the challenge is the large amount of data :
+the number of transactions can be huge.
+
+So it is difficult to have aggregated data such as volume for short period of times without too much delay, or near real time.
+
+The goal of this project is too show a way to address this issue.
+
+1. By connecting to Coinbase through their websocket, we get transactions in real time. 
+2. Transactions are injected into kafka, which process them in real time as well
+3. The volume aggregation is done by using stateful streaming technique thanks to the kafka stream api
+
+## Description of the data aggregation part ( kafka stream)
+
+In order to get bitcoin transactions volume we need to define dimensions for our aggregation.
+
+-   the first obvious one is the "side" of a transaction ( buy or sell)
+-   the second one is the period of time for bucketting our transactions, also called a "time window" : In our case we limit to one minute windows
+
+In summary, transactions are grouped by side and time window, for each group we sum the price of each transaction
+
+After each kafka event is processed a new state of the volume for a specific side and time window is emitted, and stored in a database , so we can expose later the volumes for a large time scale.
+
+for more detail check the java class [OneMinuteWindowKstream](src/main/java/org/example/OneMinuteWindowKstream.java) which contains the mechanic explained above.
+
+## description of ingestion part ( websocket -> kafka)
+
+This part is more obvious.
+A websocket connection is attached to coinbase websoket endpoint , and each event received is broadcasted to a kafka topic.
+
+please refer to [python connector](websocket/connect.py)
 
 
-## run vs prod kafka and db
+## Cloud based solution
 
-IMPORTANT : If you don't have a kafka cluster running in confluent or other cloud provider, please follow the "infrastructure as code" section to create your kafka cluster in confluence cloud before continuing.
+The infrasctructures chosen to handle the data are big query in GCP and a kafka cluster in Confluent cloud
 
-the service recquires some secrets like cluster key etc...
-you need to create a env file that contains those secret then build and run the containers
+please refer to the terraform directory to check how the infrastructure definition of both [google cloud part](terraform/google-cloud) and [confluent cloud part](terraform/confluent-cloud).
 
-### create env file
-use the template given from the ``env`` file to create your own ``.env``
-``cp env .env``
-replace cluster key, secret and url with yours ( created after applying terraform plan)
-
-### Run producer against remote kafka
-using docker in order to provide a working environment for python
-
-1- build image ``docker-compose -f docker-compose-prod.yml build --no-cache producer``
-2- build and run the docker container ``docker-compose -f docker-compose-prod.yml up producer``
-
-### Run consumer against remote kafka
-1- build image ``docker-compose -f docker-compose-prod.yml build --no-cache consumer``
-2- execute ``docker-compose -f docker-compose-prod.yml up consumer``
-
-### Run producer and consumer against remote kafka
-1- build images ``docker-compose -f docker-compose-prod.yml build``
-2 - execute ``docker-compose -f docker-compose-prod.yml up``
+for simplicity, there is no cloud infrasctructure definition or deployment to the cloud for the 2 apps that ( python and java part), so we will run them locally in docker.
 
 
 
-## infrastructure as a code
-This section allows to recreate/update a kafka cluster in confluent cloud using terraform template.
-This is important because keeping the definition of the infrastructure as code, will prevent infrastructure inconsistencies.
+## How to reproduce the solution 
 
-### create confluent cloud account
-If you already have a confluent cloud account you can skip this step.
-
-go to https://confluent.cloud/
-and follow the steps to get your account working
-you can skip the payment method and you will have a trial budget for a few week.
+### create cloud infrastructure
 
 ### install terraform
 follow instruction here https://developer.hashicorp.com/terraform/install
 
-### create a confluent api key and secret
+### infrastructure as a code - kafka
+This section allows to recreate/update a kafka cluster in confluent cloud using terraform template.
+
+#### create confluent cloud account
+If you already have a confluent cloud account you can skip this step.
+
+go to https://confluent.cloud/
+and follow the given instructions to get your account working
+you can skip the payment method and you will have a trial budget for a few week.
+
+
+#### create a confluent api key and secret
 this will be recquired for terraform to have access to your confluent could account
 go to https://confluent.cloud/settings/api-keys/create?user_dir=ASC&user_sort=status and follow the procedure.
 
 copy or download them but remind that it s security issue to keep them, but you will need them to run the terraform code.
 
-### running terraform code
+#### running terraform code
 -   go in terraform/confluent-cloud folder
 ``cd terraform/confluent-cloud/``
 -   initialise terraform
@@ -71,9 +91,62 @@ and check that everything was created from https://confluent.cloud/environments
 note that you will need the new created kafka cluster api key and secret for running apps
 they can be found in terraform.tfstate file looking for `credentials` ( if you have jq installed execute ``terraform state pull | jq '.resources[] | select(.type == "confluent_kafka_topic") | .instances[0].attributes.credentials'``)
 
+### infrastructure as a code - big query
+This section allows to recreate/update big query dataset in google cloud platform with terraform.
+
+#### create GCP account
+If you already have a google cloud account you can skip this step.
+
+go to https://console.cloud.google.com/
+and follow the given instructions to get your account working
+you have to provide payment method BUT don't worry you will have a trial budget for 3 months ( it's advised to set up an budget alert since the solution provided is quite data intensive and could reach a certain amount of paid service if you keep it running for a few days without interuption).
+
+configure gcloud cli ( required for terraform auth to gcp) https://cloud.google.com/sdk/docs/initializing
 
 
-## run all in local
+#### running terraform code
+-   go in terraform/google-cloud folder
+``cd terraform/google-cloud/``
+-   initialise terraform
+``terraform init``
+-   check the planning 
+``terraform plan``
+-   apply the plan
+``terraform apply``
+and check that everything was created from https://console.cloud.google.com/bigquery
+
+check also that the gcp-key.json as been create on your local , which is used by the java ( kafka stream) app to authenticate to gcp using the service account created in terraform
+
+
+#### build the data aggregation part
+to build the data consumer executable
+``./gradlew clean build -PcustomName=consumer -PmainClass=org.example.OneMinuteWindowKstream``
+
+
+## run local applications vs cloud based kafka and bigquery
+
+### create env file
+use the template given from the ``env`` file to create your own ``.env``
+``cp env .env``
+replace cluster key, secret and url with yours ( created after applying terraform plan) and your gcp project id
+
+### Run producer against remote kafka
+using docker in order to provide a working environment for python
+
+1- build image ``docker-compose -f docker-compose-prod.yml build --no-cache producer``
+2- build and run the docker container ``docker-compose -f docker-compose-prod.yml up producer``
+
+### Run consumer against remote kafka
+1- build image ``docker-compose -f docker-compose-prod.yml build --no-cache consumer``
+2- execute ``docker-compose -f docker-compose-prod.yml up consumer``
+
+### Run producer and consumer against remote kafka
+1- build images ``docker-compose -f docker-compose-prod.yml build``
+2 - execute ``docker-compose -f docker-compose-prod.yml up``
+
+
+## run all in local kafka ( missing bigquery part)
+if you want to use kafka locally  
 using docker in order to provide a working environment without pre-installation like java, etcc..
 
 1- build artifact ( jar file)
--- src/main/java/org/example/OneMinuteWindowKstream.java
+++ src/main/java/org/example/OneMinuteWindowKstream.java
@@ -24,103 +24,94 @@ import java.time.Instant;
 import java.util.Properties;
 
 public class OneMinuteWindowKstream {
-        private Properties props = new Properties();
-        private BigQueryWriter bigQueryWriter;
+	private Properties props = new Properties();
+	private BigQueryWriter bigQueryWriter;
 
-        public OneMinuteWindowKstream(BigQueryWriter bigQueryWriter) {
-                this.bigQueryWriter = bigQueryWriter;
-                System.out.println("url " + Secrets.SERVER_URL);
-                props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, Secrets.SERVER_URL);
-                props.put("security.protocol", "SASL_SSL");
-                props.put("sasl.jaas.config",
-                                "org.apache.kafka.common.security.plain.PlainLoginModule required username='"
-                                                + Secrets.KAFKA_CLUSTER_KEY + "' password='"
-                                                + Secrets.KAFKA_CLUSTER_SECRET + "';");
-                props.put("sasl.mechanism", "PLAIN");
-                props.put("client.dns.lookup", "use_all_dns_ips");
-                props.put("session.timeout.ms", "45000");
-                props.put(StreamsConfig.APPLICATION_ID_CONFIG, Secrets.GROUP_ID);
-                props.put(ConsumerConfig.GROUP_ID_CONFIG, Secrets.GROUP_ID);
-                props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-                props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+	public OneMinuteWindowKstream(BigQueryWriter bigQueryWriter) {
+		this.bigQueryWriter = bigQueryWriter;
+		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, Secrets.SERVER_URL);
+		props.put("security.protocol", "SASL_SSL");
+		props.put("sasl.jaas.config",
+				"org.apache.kafka.common.security.plain.PlainLoginModule required username='"
+						+ Secrets.KAFKA_CLUSTER_KEY + "' password='"
+						+ Secrets.KAFKA_CLUSTER_SECRET + "';");
+		props.put("sasl.mechanism", "PLAIN");
+		props.put("client.dns.lookup", "use_all_dns_ips");
+		props.put("session.timeout.ms", "45000");
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, Secrets.GROUP_ID);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, Secrets.GROUP_ID);
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
 
-        }
+	}
 
-        public OneMinuteWindowKstream() {
-                // TODO Auto-generated constructor stub
-        }
+	public OneMinuteWindowKstream() {
+		// TODO Auto-generated constructor stub
+	}
 
-        public Topology createTopology() {
-                StreamsBuilder streamsBuilder = new StreamsBuilder();
-                var ridesStream = streamsBuilder
-                                .stream(Topics.ORDERS_BTC,
-                                                Consumed.with(Serdes.String(),
-                                                                CustomSerdes.getSerde(OrderBtcEvent.class))
-                                                                .withTimestampExtractor(
-                                                                                (ConsumerRecord<Object, Object> record,
-                                                                                                long partitionTime) -> {
-                                                                                        System.out.println("time "
-                                                                                                        + Instant.parse(
-                                                                                                                        ((OrderBtcEvent) record
-                                                                                                                                        .value()).time)
-                                                                                                                        .toEpochMilli());
-                                                                                        return Instant.parse(
-                                                                                                        ((OrderBtcEvent) record
-                                                                                                                        .value()).time)
-                                                                                                        .toEpochMilli();
-                                                                                }))
-                                .groupByKey()
-                                .windowedBy(TimeWindows.of(Duration.ofMinutes(1)))
-                                .aggregate(
-                                                () -> new VolumeBtcTimeWindow(),
-                                                (key, value, aggregate) -> {
-                                                        aggregate.side = key;
-                                                        aggregate.addVolume(value);
-                                                        return aggregate;
-                                                },
-                                                Materialized
-                                                                .<String, VolumeBtcTimeWindow, WindowStore<Bytes, byte[]>>as(
-                                                                                "one_minute_materialised_window")
-                                                                .withKeySerde(Serdes.String())
-                                                                .withValueSerde(CustomSerdes
-                                                                                .getSerde(VolumeBtcTimeWindow.class)))
-                                .toStream()
-                                .map(
-                                                (key, value) -> new KeyValue<>(value.side + "T" + key.window().start(),
-                                                                new VolumeHourly(
-                                                                                key.window().start(),
-                                                                                key.window().end(),
-                                                                                value.side,
-                                                                                value.volume)));
+	public Topology createTopology() {
+		StreamsBuilder streamsBuilder = new StreamsBuilder();
+		var ridesStream = streamsBuilder
+				.stream(Topics.ORDERS_BTC,
+						Consumed.with(Serdes.String(),
+								CustomSerdes.getSerde(OrderBtcEvent.class))
+								.withTimestampExtractor(
+										(ConsumerRecord<Object, Object> record,
+												long partitionTime) -> Instant.parse(
+														((OrderBtcEvent) record
+																.value()).time)
+														.toEpochMilli()))
+				.groupByKey()
+				.windowedBy(TimeWindows.of(Duration.ofMinutes(1)))
+				.aggregate(
+						() -> new VolumeBtcTimeWindow(),
+						(key, value, aggregate) -> {
+							aggregate.side = key;
+							aggregate.addVolume(value);
+							return aggregate;
+						},
+						Materialized.<String, VolumeBtcTimeWindow, WindowStore<Bytes, byte[]>>as(
+								"one_minute_materialised_window")
+								.withKeySerde(Serdes.String())
+								.withValueSerde(CustomSerdes
+										.getSerde(VolumeBtcTimeWindow.class)))
+				.toStream()
+				.map(
+						(key, value) -> new KeyValue<>(value.side + "T" + key.window().start(),
+								new VolumeHourly(
+										key.window().start(),
+										key.window().end(),
+										value.side,
+										value.volume)));
 
-                ridesStream.to(Topics.ONE_MIN_WINDOW,
-                                Produced.with(
-                                                Serdes.String(),
-                                                CustomSerdes.getSerde(VolumeHourly.class)));
+		ridesStream.to(Topics.ONE_MIN_WINDOW,
+				Produced.with(
+						Serdes.String(),
+						CustomSerdes.getSerde(VolumeHourly.class)));
 
-                streamsBuilder.stream(Topics.ONE_MIN_WINDOW,
-                                Consumed.with(Serdes.String(),
-                                                CustomSerdes.getSerde(VolumeHourly.class)))
-                                .foreach((key, volumeHourly) -> {
-                                        System.out.println(volumeHourly);
-                                        bigQueryWriter.writeStream(volumeHourly);
-                                });
-                return streamsBuilder.build();
-        }
+		streamsBuilder.stream(Topics.ONE_MIN_WINDOW,
+				Consumed.with(Serdes.String(),
+						CustomSerdes.getSerde(VolumeHourly.class)))
+				.foreach((key, volumeHourly) -> {
+					System.out.println(volumeHourly);
+					bigQueryWriter.writeStream(volumeHourly);
+				});
+		return streamsBuilder.build();
+	}
 
-        public void calculateVolumeWindowed() {
-                var topology = createTopology();
-                var kStreams = new KafkaStreams(topology, props);
-                kStreams.start();
+	public void calculateVolumeWindowed() {
+		var topology = createTopology();
+		var kStreams = new KafkaStreams(topology, props);
+		kStreams.start();
 
-        }
+	}
 
-        public static void main(String[] args) {
-                System.out.println("starting stream with " + Secrets.GCP_PROJECT_ID + Secrets.DATASET
-                                + Secrets.SERVER_URL);
-                new OneMinuteWindowKstream();
-                var object = new OneMinuteWindowKstream(
-                                new BigQueryWriter(Secrets.GCP_PROJECT_ID, Secrets.DATASET, Secrets.TABLE));
-                object.calculateVolumeWindowed();
-        }
+	public static void main(String[] args) {
+		System.out.println("starting stream with " + Secrets.GCP_PROJECT_ID + Secrets.DATASET
+				+ Secrets.SERVER_URL);
+		new OneMinuteWindowKstream();
+		var object = new OneMinuteWindowKstream(
+				new BigQueryWriter(Secrets.GCP_PROJECT_ID, Secrets.DATASET, Secrets.TABLE));
+		object.calculateVolumeWindowed();
+	}
 }
